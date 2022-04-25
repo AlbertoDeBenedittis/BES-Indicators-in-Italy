1688037070a61bbce674a3c0286a3ae97ae953af
AlbertoDeBenedittis <74197291+AlbertoDeBenedittis@users.noreply.github.com>
2022-04-25 10:20:42 +0200

    Delete Dashboard3.py

--- Dashboard3.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import ee
-import folium
-import geemap.foliumap as geemap
-
-
-m = geemap.Map(center = [41.9027835,12.4963655], zoom = 6)
-m.split_map(left_layer='SATELLITE', right_layer='OpenTopoMap')
-m
